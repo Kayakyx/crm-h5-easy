85745faf1948f56cbf29667c856a4d104f2502cb
ta0hua <1522025433@qq.com>
2019-01-17 20:47:35 +0800

    简单封装axios、api统一管理

--- src/main.js
+++ src/main.js
@@ -12,7 +12,8 @@ Vue.prototype.$api = api; //将所有接口挂载的vue实例原型上。
 //把axios 进行了封装，和接口的统一管理，所以这里就不用  Vue.use(VueAxios, axios)， 特别
 // 是VueAxios,主要的作用就是把 axios挂载到vue实例上，和自己用Vue.
 // Vue.use(VueAxios, axios)
-Vue.use(Button,Toast)
+Vue.use(Button)
+Vue.use(Toast)
 Vue.config.productionTip = false
 new Vue({
   router,
